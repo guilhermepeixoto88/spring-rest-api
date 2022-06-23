b3bde6734964228298351bfdf0be4b7c13e38f2e
guilherme.peixoto <guilherme.peixoto88@gmail.com>
2022-06-22 21:45:02 -0300

    Changing post request on users to return http 201 and created resource uri

--- src/main/java/com/github/rest/webservices/springrestapi/user/UserResource.java
+++ src/main/java/com/github/rest/webservices/springrestapi/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.github.rest.webservices.springrestapi.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,7 +29,15 @@ public class UserResource {
 	}
 	
 	@PostMapping(path="/users")
-	public void create(@RequestBody User user) {
+	public ResponseEntity<Object> create(@RequestBody User user) {
 		User createdUser = userDao.save(user);
+		
+		URI uri = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(createdUser.getId())
+			.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
