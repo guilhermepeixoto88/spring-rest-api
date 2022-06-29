85afb3f4d58e06bb2240e83093d0b0e030dbc4b9
guilherme.peixoto <guilherme.peixoto88@gmail.com>
2022-06-29 07:03:30 -0300

    Internationalized hello world

--- src/main/java/com/github/rest/webservices/springrestapi/helloworld/HelloWorldController.java
+++ src/main/java/com/github/rest/webservices/springrestapi/helloworld/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.github.rest.webservices.springrestapi.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
@@ -20,5 +26,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean/{name}")
 	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("hello.world.message", null, "Default Message" , LocaleContextHolder.getLocale());
 	}
 }
